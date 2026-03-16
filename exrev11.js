c965c295e49a86299f9264eb2af1d6ec6105c862
Arthur M.Pioli <arthur.maximino.oliveira@escola.pr.gov.br>
2026-03-16 10:55:02 -0300

    Add files via upload

---
+++ exrev11.js
@@ -0,0 +1,8 @@
+//Crie um programa para mostrar se um número é maior que 22.
+
+let num = 20;
+if (num > 22){
+    console.log(`O número é maior que 22`)
+} else{
+    console.log(`O número é menor que 22`)
+}
---
+++ exrev12.js
@@ -0,0 +1,8 @@
+//Criar um programa para verificar se um número é multiplo de 5.
+
+let num = 20;
+if (num % 5 === 0){
+    console.log(`O número é múltiplo de 5`)
+} else{
+    console.log(`O número não é múltiplo de 5`)
+}
