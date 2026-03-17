e69e2b499b6f619d82fbe89c12268da57063e045
Arthur M.Pioli <arthur.maximino.oliveira@escola.pr.gov.br>
2026-03-17 11:39:46 -0300

    Update exrev17.js

--- exrev17.js
+++ exrev17.js
@@ -1,14 +1,14 @@
 //Crie um programa com uma condicional composta para avaliar o desempenho escolar entre "Excelente", "Ótimo", "Bom", "Regular" ou "Ruim". Dependendo da nota.
 
-let nota = 50;
+let nota = 20;
 if (nota >= 90) {
     console.log(`Excelente`);
-} else if(nota >= 80) {
+} else if(nota <= 89 && nota >= 75) {
     console.log (`Ótimo`);
-} else if (nota >= 70) {
+} else if (nota <= 74 && nota >= 65) {
     console.log(`Bom`);
-} else if(nota >= 60) {
+} else if(nota <= 64 && nota >= 55) {
     console.log(`Regular`);
-} else if(nota <= 59) {
+} else if(nota <= 54) {
     console.log(`Ruim`);
 }
